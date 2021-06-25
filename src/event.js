ea94d809c2e42bb844113a9d04abb0d18143113b
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-06-26 00:46:10 +0700

    3.Event

---
+++ src/event.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Event() {
+  function Greeting() {
+    return alert("good morning everyone have a nice day");
+  }
+
+  return (
+    <div>
+      <p>If you press Click Here then an alert will appear</p>
+      <button onClick={() => alert("Hello full-stack bootcamp participants")}>
+        Click Here
+      </button>
+
+      <p>If you press Greeting then an alert will appear</p>
+      <button onClick={Greeting}>Greeting</button>
+    </div>
+  );
+}
+
+export default Event;
