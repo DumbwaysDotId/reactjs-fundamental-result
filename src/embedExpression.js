9096061748bb7001a1bf9b09a9a0886cf071cc60
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-06-26 00:34:08 +0700

    2.Embedding-Expression

---
+++ src/embedExpression.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function EmbedExpression() {
+  //Create function here
+  function getMajor() {
+    return " Full-Stack";
+  }
+
+  //Make a variable here
+  const companyName = "Dumbways.id";
+
+  return (
+    <div className="App-header">
+      {/*Include expression in a variable*/}
+      <p>
+        Welcome To {companyName} Class
+        {/*Include expression in a function*/}
+        {getMajor()}
+      </p>
+    </div>
+  );
+}
+
+export default EmbedExpression;
